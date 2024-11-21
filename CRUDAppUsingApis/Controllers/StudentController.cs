bc9aba5ed5ce106dba39d19f994173a0543b0478
Obaydullah <obaydullahkhaan@gmail.com>
2024-11-22 02:18:24 +0600

    Add project files.

---
+++ CRUDAppUsingApis/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDAppUsingApis.Controllers
+{
+    public class StudentController : Controller
+    {
+        private string url = "https://localhost:7134/api/Students/";
+        private HttpClient client = new HttpClient();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            Lis
+            return View();
+        }
+    }
+}
---
+++ CRUDAppUsingApis/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿
+namespace CRUDAppUsingApis.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string studentName { get; set; }
+        public string studentGender { get; set; }
+        public int age { get; set; }
+        public int standard { get; set; }
+    }
+}
