7f91b9450489e869a15e9336ff323c7961eba819
Obaydullah <obaydullahkhaan@gmail.com>
2024-11-22 03:33:09 +0600

    crud added using web apis

--- CRUDAppUsingApis/Models/Student.cs
+++ CRUDAppUsingApis/Models/Student.cs
@@ -1,12 +1,18 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace CRUDAppUsingApis.Models
 {
     public class Student
     {
         public int id { get; set; }
+        [Required]
         public string studentName { get; set; }
+        [Required]
         public string studentGender { get; set; }
+        [Required]
         public int age { get; set; }
+        [Required]
         public int standard { get; set; }
     }
 }
